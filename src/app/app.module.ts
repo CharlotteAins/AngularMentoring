0a9e626ca3554f62d60590db457043595eaedbec
Viktoryia_Naumava@epam.com <viktoryia_naumava@epam.com>
2020-10-04 21:23:40 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogoComponent } from './logo/logo.component';
+import { CoursesPageComponent } from './courses-page/courses-page.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { CoursesItemComponent } from './courses-item/courses-item.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    CoursesPageComponent,
+    BreadcrumbsComponent,
+    CoursesListComponent,
+    CoursesItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
